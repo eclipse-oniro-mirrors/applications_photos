07533ef9c425c19980d25901550e30e831f001c3
Fanny <1179195791@qq.com>
2022-09-16 10:38:59 +0800

    Signed-off-by: Fanny <1179195791@qq.com>
     Changes to be committed:

--- entry/src/main/ets/feature/editor/PhotoEditorManager.ts
+++ entry/src/main/ets/feature/editor/PhotoEditorManager.ts
@@ -62,7 +62,7 @@ export class PhotoEditorManager {
 
     clear() {
         this.logger.debug('clear');
-        this.editors[this.currentMode].exit();
+        this.editors[this.currentMode] && this.editors[this.currentMode].exit();
         this.item = undefined;
         this.origin && this.origin.release();
         this.origin = undefined;
