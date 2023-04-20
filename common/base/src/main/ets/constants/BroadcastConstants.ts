e27795935a65874452bcaaa7adf6a12684547b0b
liujuan <liujuan76@h-partners.com>
2023-04-20 08:44:22 +0000

    update common/base/src/main/ets/constants/BroadcastConstants.ts.
    Signed-off-by: liujuan <liujuan76@h-partners.com>

--- common/base/src/main/ets/constants/BroadcastConstants.ts
+++ common/base/src/main/ets/constants/BroadcastConstants.ts
@@ -56,5 +56,6 @@ export class BroadcastConstants {
     static readonly CHANGE_SWIPER_DURATION = 'change_swiper_duration';
     // USED for AppStorage
     static readonly LEFT_BLANK: string = 'leftBlank';
-    static readonly RESET_TABBAR_SELECTED_STATUE: string = 'reset_tabbar_selected_statue';
+    static readonly RESET_TAB_SELECTED_STATUE: string = 'reset_tab_selected_statue';
+    static readonly RESET_TAB_SELECTED_TEXT: string = 'reset_tab_selected_text';
 }
