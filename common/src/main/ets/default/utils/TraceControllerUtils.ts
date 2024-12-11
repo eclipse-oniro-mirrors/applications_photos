c32828faba2f998186e54fb76b524581797519df
jiangwei <jiangwei214@h-partners.com>
2024-12-11 15:05:41 +0800

    fix：bytrace日落，使用hitrace方式采集 Signed-off-by:jiangwei <jiangwei214@h-partners.com>
    Signed-off-by: jiangwei <jiangwei214@h-partners.com>

--- common/src/main/ets/default/utils/TraceControllerUtils.ts
+++ common/src/main/ets/default/utils/TraceControllerUtils.ts
@@ -13,27 +13,27 @@
  * limitations under the License.
  */
 
-import bytrace from '@ohos.bytrace';
+import hiTraceMeter from '@ohos.hiTraceMeter';
 
 export class TraceControllerUtils {
   // Used to track the start of sequential tasks
   public static startTrace(name: string): void {
-    bytrace.startTrace(name, 1);
+    hiTraceMeter.startTrace(name, 1);
   }
 
   // Used to track the end of sequential tasks. The name must be the same as that at start
   public static finishTrace(name: string): void {
-    bytrace.finishTrace(name, 1);
+    hiTraceMeter.finishTrace(name, 1);
   }
 
   // Used to track the start of cross tasks
   // (scenarios where the next task starts before the previous task is finished)
   public static startTraceWithTaskId(name: string, taskId: number): void {
-    bytrace.startTrace(name, taskId);
+    hiTraceMeter.startTrace(name, taskId);
   }
 
   // Used to track the end of cross tasks. The name and taskid must be the same as at start
   public static finishTraceWithTaskId(name: string, taskId: number): void {
-    bytrace.finishTrace(name, taskId);
+    hiTraceMeter.finishTrace(name, taskId);
   }
 }
